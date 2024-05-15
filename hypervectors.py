475d054dd23a8851705770f708c76e6ae74d3487
Jonas Schmidt <jonas.schmidt1@louisiana.edu>
2024-05-15 13:40:20 -0500

    building upon Hypervector class

--- hypervectors.py
+++ hypervectors.py
@@ -1,10 +1,23 @@
+# TODO: docstrings
+# TODO: support for binary and polar Hypervectors?
+
 import tensorflow as tf
 from typing import Optional, Tuple
 
 class Hypervector:
-    def __init__(self, value: int, size: int, value_range: Optional[Tuple[int, int]] = None):
-        self.value = value
+    '''
+    A class to represent a Hypervector.
+
+    Attributes:
+        size (int): The size of the Hypervector
+        value (int): The value represented by the Hypervector
+        value_range (Tuple[int, int]): Typically (0, size), is used when generating the tensor and is most useful when the Hypervector can represent negative numbers
+        tensor (tensorflow.python.framework.ops.EagerTensor): The actual Hypervector, represented as a tensor
+    '''
+
+    def __init__(self, size: int, value: Optional[int] = 0, value_range: Optional[Tuple[int, int]] = None):
         self.size = size
+        self.value = value
         self.tensor = self.generate_tensor(value_range)
 
     def generate_tensor(self, value_range: Optional[Tuple[int, int]] = None) -> tf.Tensor:
@@ -20,11 +33,16 @@ class Hypervector:
         updates = tf.ones(num_ones, dtype=tf.int32)
 
         return tf.tensor_scatter_nd_update(corr_vec, tf.expand_dims(indices, 1), updates)
-    
-    def regen_tensor(self) -> None:
-        # Shuffle the indices again to ensure randomness
-        indices = tf.random.shuffle(tf.range(self.size))[:tf.reduce_sum(self.tensor == 1)]
-        updates = tf.ones(tf.shape(indices), dtype=tf.int32)
-        self.tensor = tf.tensor_scatter_nd_update(self.tensor, tf.expand_dims(indices, 1), updates)
 
+    # TODO: def regen_tensor()
 
+    def binarize(self):
+        self.tensor = tf.where(self.tensor > 0, tf.ones_like(self.tensor), tf.zeros_like(self.tensor))
+
+    def polarize(self):
+        self.tensor = tf.where(self.tensor <= 0, tf.constant(-1, dtype=tf.int32), tf.ones_like(self.tensor))
+
+    # returns [-1, 1], with 0 denoting complete orthogonality
+    @staticmethod
+    def cos_similarity(hv1, hv2):
+        return (tf.keras.losses.cosine_similarity(tf.cast(hv1.tensor, dtype=tf.float32), tf.cast(hv2.tensor, dtype=tf.float32))).numpy()    
--- test.py
+++ test.py
@@ -2,6 +2,11 @@ import tensorflow as tf
 
 from hypervectors import Hypervector
 
-hv = Hypervector(value=10, size=256)
+hv_0 = Hypervector(size=256)
 
-print(hv.tensor)
+hv_1 = Hypervector(size=256, value=256)
+
+hv_r1 = Hypervector(size=256, value=128)
+hv_r2 = Hypervector(size=256, value=128)
+
+print(help(Hypervector))
